9d105560934ab9fcae7257ca9706a1167bca8f9f
Anthony <aquinoanthony851@gmail.com>
2024-05-10 00:08:31 -0600

    Password protection using Bcrypt

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 from flask import request, session
 from flask_restful import Resource
 
-from config import app, db, api
+from config import app, db, api, bcrypt
 from models import User
 
 class ClearSession(Resource):
@@ -27,17 +27,39 @@ class Signup(Resource):
         db.session.commit()
         return user.to_dict(), 201
 
+from flask import jsonify
+
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter_by(id=user_id).first()
+            if user:
+                return jsonify(user.to_dict())
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        username = json.get('username')
+        password = json.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return jsonify(user.to_dict())
+        return {}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
